36f6f40cf0301e5734ea006cea02c33e157a4b99
Chinyaa13s <137972903+Chinyaa13s@users.noreply.github.com>
2026-08-16 17:05:31 +0530

    Enable Cashfree payment routes

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
+
 const {
   getStripeKey,
   createPaymentIntent,
   confirmPayment,
 } = require('../controllers/paymentController');
-const { createRazorpayOrder, verifyPayment } = require('../controllers/razorpayController');
-// const { createCashfreeOrder, verifyCashfreeOrder } = require('../controllers/cashfreeController');
+
+const {
+  createRazorpayOrder,
+  verifyPayment,
+} = require('../controllers/razorpayController');
+
+const {
+  createCashfreeOrder,
+  verifyCashfreeOrder,
+} = require('../controllers/cashfreeController');
+
 const { protect } = require('../middleware/auth');
 
 router.use(protect);
@@ -20,6 +30,7 @@ router.post('/razorpay/create-order', createRazorpayOrder);
 router.post('/razorpay/verify', verifyPayment);
 
 // Cashfree routes
-// router.post('/cashfree/create-order', createCashfreeOrder);
-// router.post('/cashfree/verify', verifyCashfreeOrder);
+router.post('/cashfree/create-order', createCashfreeOrder);
+router.post('/cashfree/verify', verifyCashfreeOrder);
+
 module.exports = router;
